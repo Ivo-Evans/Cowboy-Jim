81c7f6b3ed5b9190a858c0df0bde7f37f50cfb45
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-04 17:53:10 +0000

    add randomly generated running ninjas from top to bottom

--- cowboy_jim.js
+++ cowboy_jim.js
@@ -1,11 +1,21 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let jimsDirection = 'up';
+let enemies = {
+    top: [],
+    right: [],
+    bottom: [],
+    left: []
+}
+let enemyRate = 0.002;
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawJim(jimsDirection);
     drawBuildings();
+    generateEnemies();
+    drawEnemies();
 }
 
 function drawJim(direction) {
@@ -31,6 +41,28 @@ function drawBuilding(x, y) {
     ctx.closePath;
 }
 
+function generateEnemies() {
+   let chance = Math.random();
+   if (chance > enemyRate) { 
+       return
+    } else {
+        enemies.top.push({x: 325, y: 0})
+    } 
+}
 
+function drawEnemies() {
+    enemies.top.forEach(enemy => {
+        renderNinja(enemy);
+        enemy.y += 1;
+    })    
+}
+
+function renderNinja(ninja) {
+    ctx.beginPath()
+    ctx.rect(ninja.x, ninja.y, 30, 30)
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+}
 
 let interval = setInterval(draw, 5);
